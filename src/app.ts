deb88824b3def7175dddee063ffd5936c5648a45
sourob2356 <sourob2356@gmail.com>
2025-12-13 18:04:17 +0600

    Initialize project setup

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
+import config from './config';
+
+const app: Application = express();
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({
+        Message: "Server is running..",
+        environment : config.node_env,
+        uptime : process.uptime().toFixed() + ' sec'
+    })
+});
+
+
+app.use(globalErrorHandler);
+
+app.use(notFound);
+
+export default app;
